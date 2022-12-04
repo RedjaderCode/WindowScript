5b765c20c39a30a20178608b0306b926324daaf7
Joshua Luijkx <108335919+RedjaderCode@users.noreply.github.com>
2022-12-04 20:44:05 +0100

    Update WindowScript.cpp

--- WindowScript/WindowScript.cpp
+++ WindowScript/WindowScript.cpp
@@ -12,8 +12,8 @@ const uint16_t nScreenWidth = NULL;
 const uint16_t nScreenHeight = NULL;
 
 size_t _copy_file(const char * _file, FILE *_f, FILE *_fw){
+	// Create a better way to store the buffer. content not displayed correctly. format? insufficient memory //
     fopen_s(&_f, _file, "rb" );
-    fopen_s(&_fw, "window.config", "wb");
     fseek  (_f, 0, SEEK_END );
     size_t size_of_file = ftell(_f);
     char * buffer       = new char[size_of_file];
@@ -22,15 +22,17 @@ size_t _copy_file(const char * _file, FILE *_f, FILE *_fw){
     fread(buffer, sizeof(char), size_of_file, _f);
     std::cout << buffer;
 
-    fseek(_fw, 0, SEEK_SET);
-    fwrite(buffer, sizeof(char), size_of_file, _fw);
-
     return size_of_file;
     delete[] buffer;
 }
 
 inline void start(const char * _file, FILE *_f, FILE *_fw){
 	const size_t size_of_file = _copy_file(_file, _f, _fw);
+	
+	/* Create a string buffer to store the dynamic buffer into */
+	/* Create a function to store 'function(start of string in element access form, end of string)' into a string */
+		// note this has to be done step by step based on conditionals
+	
 
     std::cout << "\n\nconfiguration process finished! press ESC to close..." << std::endl;
 	while(1){ if(GetAsyncKeyState(VK_ESCAPE & 0x1B)) break; }
